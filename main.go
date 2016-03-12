a9ae6f1b0187d7f0db328f72e0e66fc4542b90fb
Quincy Bowers <quincy@quakbo.com>
2016-03-11 22:05:17 -0700

    Updating to run on Windows.
    Fixed some issues with when files need to be closed when moving a file.
    
    gofmt

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "os/user"
-    "path/filepath"
-    "regexp"
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"regexp"
 
-    "github.com/quincy/configo"
-    "github.com/quincy/photo_spool/spooler"
-    "github.com/quincy/photo_spool/util"
+	"github.com/quincy/configo"
+	"github.com/quincy/photo_spool/spooler"
+	"github.com/quincy/photo_spool/util"
 )
 
 var db map[string][]string = make(map[string][]string, 100)
@@ -26,41 +26,36 @@ var noop bool
 setup configuration options
 */
 func init() {
-    var err error
-    if currentUser, err = user.Current(); err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	if currentUser, err = user.Current(); err != nil {
+		log.Fatal(err)
+	}
 
-    // Setup paths.
-    //    spoolPath = filepath.Join(currentUser.HomeDir, "spool")            // TODO these should be configurable values.
-    //    basePhotoPath := filepath.Join(currentUser.HomeDir, "Pictures")    // TODO these should be configurable values.
-    //    errorPath := filepath.Join(currentUser.HomeDir, "spool_error")     // TODO these should be configurable values.
-    //    md5DbPath := filepath.Join(currentUser.HomeDir, ".photo-spool.db") // TODO these should be configurable values.
-    configo.StringVar(&spoolPath, "spooldir", filepath.Join(currentUser.HomeDir, "spool"), "The directory to look in for new pictures to spool.")
-    configo.StringVar(&basePhotoPath, "photodir", filepath.Join(currentUser.HomeDir, "Pictures"), "The directory that new pictures will be copied to as they are spooled.")
-    configo.StringVar(&errorPath, "errordir", filepath.Join(currentUser.HomeDir, "spool_error"), "The directory to copy pictures to when an error occurs.")
-    configo.StringVar(&md5DbPath, "dbdir", filepath.Join(currentUser.HomeDir, ".photo-spool.db"), "The full path to the photo database file.")
-    configo.BoolFlagVar(&noop, "dryrun", false, "If set the program has no effect but prints what would have happened.")
+	configo.StringVar(&spoolPath, "spooldir", filepath.Join(currentUser.HomeDir, "spool"), "The directory to look in for new pictures to spool.")
+	configo.StringVar(&basePhotoPath, "photodir", filepath.Join(currentUser.HomeDir, "Pictures"), "The directory that new pictures will be copied to as they are spooled.")
+	configo.StringVar(&errorPath, "errordir", filepath.Join(currentUser.HomeDir, "spool_error"), "The directory to copy pictures to when an error occurs.")
+	configo.StringVar(&md5DbPath, "dbdir", filepath.Join(currentUser.HomeDir, ".photo-spool.db"), "The full path to the photo database file.")
+	configo.BoolFlagVar(&noop, "dryrun", false, "If set the program has no effect but prints what would have happened.")
 
-    if err = configo.Parse(); err != nil {
-        panic(err)
-    }
+	if err = configo.Parse(); err != nil {
+		panic(err)
+	}
 
-    if err := os.MkdirAll(errorPath, 0775); err != nil {
-        log.Fatal(err)
-    }
+	if err := os.MkdirAll(errorPath, 0775); err != nil {
+		log.Fatal(err)
+	}
 
-    if err := os.MkdirAll(basePhotoPath, 0775); err != nil {
-        log.Fatal(err)
-    }
+	if err := os.MkdirAll(basePhotoPath, 0775); err != nil {
+		log.Fatal(err)
+	}
 
-    if err := os.MkdirAll(spoolPath, 0775); err != nil {
-        log.Fatal(err)
-    }
+	if err := os.MkdirAll(spoolPath, 0775); err != nil {
+		log.Fatal(err)
+	}
 
-    if spool, err = spooler.New(md5DbPath, basePhotoPath, errorPath, noop); err != nil {
-        log.Fatalf("Could not create a new Spool. %v\n", err)
-    }
+	if spool, err = spooler.New(md5DbPath, basePhotoPath, errorPath, noop); err != nil {
+		log.Fatalf("Could not create a new Spool. %v\n", err)
+	}
 }
 
 /*
@@ -69,53 +64,53 @@ directory then it is ignored.  Otherwise the path is sent to the items channel
 to be processed by the processFiles goroutine.
 */
 func visit(filePath string, f os.FileInfo, err error) error {
-    log.Println("Visiting", filePath)
+	log.Println("Visiting", filePath)
 
-    if !f.IsDir() {
-        pattern := "(?i:jpe?g$)"
-        matched, err := regexp.MatchString(pattern, filePath)
-        if err != nil {
-            log.Fatalf("Error compiling regular expression '%s'.  %v", pattern, err)
-        }
+	if !f.IsDir() {
+		pattern := "(?i:jpe?g$)"
+		matched, err := regexp.MatchString(pattern, filePath)
+		if err != nil {
+			log.Fatalf("Error compiling regular expression '%s'.  %v", pattern, err)
+		}
 
-        if matched {
-            spoolError := spool.Spool(filePath)
-            if spoolError != nil {
-                log.Println(spoolError)
-            }
+		if matched {
+			spoolError := spool.Spool(filePath)
+			if spoolError != nil {
+				log.Println(spoolError)
+			}
 
-            parent := filepath.Dir(filePath)
-            if util.DirIsEmpty(parent) {
-                log.Printf("Pruning empty directory [%s].\n", parent)
-                if noop {
-                    log.Println("DRY RUN Skipping delete directory.")
-                } else {
-                    os.Remove(parent)
-                }
-            }
-        } else {
-            log.Printf("Found unhandled file type [%s].  Moving the file to %s.\n", filePath, spool.ErrorPath)
-            if noop {
-                log.Println("DRY RUN Skipping move file.")
-            } else {
-                util.MoveTo(spool.ErrorPath, filePath)
-            }
-        }
-    }
+			parent := filepath.Dir(filePath)
+			if util.DirIsEmpty(parent) {
+				log.Printf("Pruning empty directory [%s].\n", parent)
+				if noop {
+					log.Println("DRY RUN Skipping delete directory.")
+				} else {
+					os.Remove(parent)
+				}
+			}
+		} else {
+			log.Printf("Found unhandled file type [%s].  Moving the file to %s.\n", filePath, spool.ErrorPath)
+			if noop {
+				log.Println("DRY RUN Skipping move file.")
+			} else {
+				util.MoveTo(spool.ErrorPath, filePath)
+			}
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func main() {
-    // Start the file walk.
-    err := filepath.Walk(spoolPath, visit)
+	// Start the file walk.
+	err := filepath.Walk(spoolPath, visit)
 
-    if err != nil {
-        fmt.Println("There was an error: ", err)
-    }
+	if err != nil {
+		fmt.Println("There was an error: ", err)
+	}
 
-    // Write out the database.
-    if err := spool.Close(); err != nil {
-        panic(err)
-    }
+	// Write out the database.
+	if err := spool.Close(); err != nil {
+		panic(err)
+	}
 }
--- spooler/spooler.go
+++ spooler/spooler.go
@@ -1,177 +1,184 @@
 package spooler
 
 import (
-    "bufio"
-    "crypto/md5"
-    "encoding/json"
-    "errors"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "log"
-    "os"
-    "path"
-    "path/filepath"
-    "strconv"
-    "strings"
-    "time"
+	"bufio"
+	"crypto/md5"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 
-    "github.com/quincy/photo_spool/util"
-    "github.com/rwcarlsen/goexif/exif"
+	"github.com/quincy/photo_spool/util"
+	"github.com/rwcarlsen/goexif/exif"
 )
 
 type Spool struct {
-    Destination string
-    dbPath      string
-    ErrorPath   string
-    db          map[string][]string
-    noop        bool
+	Destination string
+	dbPath      string
+	ErrorPath   string
+	db          map[string][]string
+	noop        bool
 }
 
 // New creates and returns a new *Spool.
 func New(dbPath, destination, errorPath string, noop bool) (*Spool, error) {
-    sp := new(Spool)
-    sp.noop = noop
+	sp := new(Spool)
+	sp.noop = noop
 
-    if !util.Exists(destination) {
-        if _, err := os.Create(destination); err != nil {
-            return nil, err
-        }
-    }
-    sp.Destination = destination
+	if !util.Exists(destination) {
+		if _, err := os.Create(destination); err != nil {
+			return nil, err
+		}
+	}
+	sp.Destination = destination
 
-    if !util.Exists(errorPath) {
-        if _, err := os.Create(errorPath); err != nil {
-            return nil, err
-        }
-    }
-    sp.ErrorPath = errorPath
+	if !util.Exists(errorPath) {
+		if _, err := os.Create(errorPath); err != nil {
+			return nil, err
+		}
+	}
+	sp.ErrorPath = errorPath
 
-    sp.dbPath = dbPath
-    if err := sp.readDatabase(); err != nil {
-        return nil, err
-    }
+	sp.dbPath = dbPath
+	if err := sp.readDatabase(); err != nil {
+		return nil, err
+	}
 
-    return sp, nil
+	return sp, nil
 }
 
 // readDatabase deserializes the md5 json database from the given filePath.
 func (sp *Spool) readDatabase() error {
-    var db map[string][]string
+	var db map[string][]string
 
-    if util.Exists(sp.dbPath) {
-        json_bytes, err := ioutil.ReadFile(sp.dbPath)
+	if util.Exists(sp.dbPath) {
+		json_bytes, err := ioutil.ReadFile(sp.dbPath)
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        err = json.Unmarshal(json_bytes, &db)
-    } else {
-        db = make(map[string][]string)
-    }
+		err = json.Unmarshal(json_bytes, &db)
+	} else {
+		db = make(map[string][]string)
+	}
 
-    sp.db = db
-    return nil
+	sp.db = db
+	return nil
 }
 
 // writeDatabase serializes the md5 database to json and writes it to disk.
 func (sp *Spool) writeDatabase() error {
-    if sp.noop {
-        log.Println("DRY RUN Skipping write database.")
-    }
+	if sp.noop {
+		log.Println("DRY RUN Skipping write database.")
+	}
 
-    b, err := json.Marshal(sp.db)
-    if err != nil {
-        return err
-    }
+	b, err := json.Marshal(sp.db)
+	if err != nil {
+		return err
+	}
 
-    err = ioutil.WriteFile(sp.dbPath, b, 0644)
-    return err
+	err = ioutil.WriteFile(sp.dbPath, b, 0644)
+	return err
 }
 
 func (sp *Spool) Close() error {
-    return sp.writeDatabase()
+	return sp.writeDatabase()
 }
 
 func (sp *Spool) Spool(file string) error {
-    // calculate an md5 sum for the file
-    hash := getHash(file)
+	// calculate an md5 sum for the file
+	hash := getHash(file)
 
-    // get the Time from the DateTimeOriginal exif tag
-    dateTime, err := getDateTime(file)
-    if err != nil {
-        log.Printf("Could not read the DateTimeOriginal tag. %v", err)
-        log.Printf("Moving %s to %s.\n", file, sp.ErrorPath)
-        if sp.noop {
-            log.Println("DRY RUN Skipping move file.")
-        } else if mverr := util.MoveTo(sp.ErrorPath, file); mverr != nil {
-            log.Fatal(mverr)
-        }
-        return err
-    }
+	// get the Time from the DateTimeOriginal exif tag
+	dateTime, err := getDateTime(file)
+	if err != nil {
+		log.Printf("Could not read the DateTimeOriginal tag. %v", err)
+		log.Printf("Moving %s to %s.\n", file, sp.ErrorPath)
+		if sp.noop {
+			log.Println("DRY RUN Skipping move file.")
+		} else if mverr := util.MoveTo(sp.ErrorPath, file); mverr != nil {
+			log.Fatal(mverr)
+		}
+		return err
+	}
 
-    // check if the hash already exists in the db
-    if _, exists := sp.db[hash]; exists {
-        msg := "A db entry already exists for " + file + "."
-        errorName := filepath.Join(sp.ErrorPath, strings.Join([]string{filepath.Base(file), "DUPLICATE"}, "."))
-        log.Printf("Mv(%s, %s)\n", errorName, file)
-        if sp.noop {
-            log.Println("DRY RUN Skipping move file.")
-        } else {
-            util.Mv(errorName, file)
-        }
-        return errors.New(msg)
-    }
+	// check if the hash already exists in the db
+	if _, exists := sp.db[hash]; exists {
+		msg := "A db entry already exists for " + file + "."
+		errorName := filepath.Join(sp.ErrorPath, strings.Join([]string{filepath.Base(file), "DUPLICATE"}, "."))
+		log.Printf("Mv(%s, %s)\n", errorName, file)
+		if sp.noop {
+			log.Println("DRY RUN Skipping move file.")
+		} else {
+			err := util.Mv(errorName, file)
+			if err != nil {
+				return err
+			}
+		}
+		return errors.New(msg)
+	}
 
-    // calculate the path to copy the file to, including a new file name
-    newPath := sp.getDestination(file, sp.Destination, dateTime)
+	// calculate the path to copy the file to, including a new file name
+	newPath := sp.getDestination(file, sp.Destination, dateTime)
 
-    // ensure the new path doesn't already exist
-    for util.Exists(newPath) {
-        dateTime = dateTime.Add(1 * time.Second)
-        newPath = sp.getDestination(file, sp.Destination, dateTime)
-    }
+	// ensure the new path doesn't already exist
+	for util.Exists(newPath) {
+		dateTime = dateTime.Add(1 * time.Second)
+		newPath = sp.getDestination(file, sp.Destination, dateTime)
+	}
 
-    log.Printf("Mv(%s, %s)\n", newPath, file)
-    if util.Exists(newPath) {
-        fields := strings.Split(filepath.Base(file), ".")
-        errorName := filepath.Join(sp.ErrorPath, strings.Join([]string{fields[0], filepath.Base(newPath)}, "::"))
-        if sp.noop {
-            log.Println("DRY RUN Skipping move file.")
-        } else {
-            util.Mv(errorName, file)
-        }
-        msg := "A file with that named " + newPath + " already exists at the destination.  Moving to " + errorName
-        log.Println(msg) // TODO This logging sucks.
-        // TODO send an e-mail.
-        return errors.New(msg)
-    }
+	log.Printf("Mv(%s, %s)\n", newPath, file)
+	if util.Exists(newPath) {
+		fields := strings.Split(filepath.Base(file), ".")
+		errorName := filepath.Join(sp.ErrorPath, strings.Join([]string{fields[0], filepath.Base(newPath)}, "::"))
+		if sp.noop {
+			log.Println("DRY RUN Skipping move file.")
+		} else {
+			err := util.Mv(errorName, file)
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+		}
+		msg := "A file with that named " + newPath + " already exists at the destination.  Moving to " + errorName
+		log.Println(msg) // TODO This logging sucks.
+		// TODO send an e-mail.
+		return errors.New(msg)
+	}
 
-    // move the file to its new home
-    if sp.noop {
-        log.Println("DRY RUN Skipping move file.")
-    } else if err := util.Mv(newPath, file); err != nil {
-        log.Println(err)
-        return err
-    }
+	// move the file to its new home
+	if sp.noop {
+		log.Println("DRY RUN Skipping move file.")
+	} else if err := util.Mv(newPath, file); err != nil {
+		log.Println(err)
+		return err
+	}
 
-    // add an entry to the hashmap db
-    if err := sp.insertDb(newPath, hash); err != nil {
-        return err // TODO plain errors suck...
-    }
+	// add an entry to the hashmap db
+	if err := sp.insertDb(newPath, hash); err != nil {
+		return err // TODO plain errors suck...
+	}
 
-    return nil
+	return nil
 }
 
 func (sp *Spool) insertDb(file, hash string) error {
-    if _, exists := sp.db[hash]; !exists {
-        sp.db[hash] = make([]string, 0, 5)
-    }
+	if _, exists := sp.db[hash]; !exists {
+		sp.db[hash] = make([]string, 0, 5)
+	}
 
-    sp.db[hash] = append(sp.db[hash], file)
+	sp.db[hash] = append(sp.db[hash], file)
 
-    return nil
+	return nil
 }
 
 /*
@@ -181,26 +188,26 @@ representation.
 TODO Move to its own package?
 */
 func getHash(filePath string) string {
-    log.Println("Entering getHash(", filePath, ")")
-    h := md5.New()
+	log.Println("Entering getHash(", filePath, ")")
+	h := md5.New()
 
-    inputFile, inputError := os.Open(filePath)
-    if inputError != nil {
-        log.Printf("An error occurred while opening the input file [%s].\n", filePath)
-    }
-    defer inputFile.Close()
+	inputFile, inputError := os.Open(filePath)
+	if inputError != nil {
+		log.Printf("An error occurred while opening the input file [%s].\n", filePath)
+	}
+	defer inputFile.Close()
 
-    inputReader := bufio.NewReader(inputFile)
-    inputString, readerError := inputReader.ReadString('\n')
-    for readerError != io.EOF {
-        io.WriteString(h, inputString)
-        inputString, readerError = inputReader.ReadString('\n')
-    }
+	inputReader := bufio.NewReader(inputFile)
+	inputString, readerError := inputReader.ReadString('\n')
+	for readerError != io.EOF {
+		io.WriteString(h, inputString)
+		inputString, readerError = inputReader.ReadString('\n')
+	}
 
-    sum := fmt.Sprintf("%x", h.Sum(nil))
+	sum := fmt.Sprintf("%x", h.Sum(nil))
 
-    log.Println("Returning ", sum, " from getHash(", filePath, ")")
-    return sum
+	log.Println("Returning ", sum, " from getHash(", filePath, ")")
+	return sum
 }
 
 /*
@@ -208,28 +215,32 @@ getDateTime reads the exif data from fname and returns a string representation
 of the DateTimeOriginal tag.
 */
 func getDateTime(fname string) (time.Time, error) {
-    f, err := os.Open(fname)
-    if err != nil {
-        return time.Now(), err
-    }
+	f, err := os.Open(fname)
+	if err != nil {
+		return time.Now(), err
+	}
+	defer f.Close()
 
-    x, err := exif.Decode(f)
-    if err != nil {
-        return time.Now(), err
-    }
+	x, err := exif.Decode(f)
+	if err != nil {
+		return time.Now(), err
+	}
 
-    date, err := x.Get(exif.DateTimeOriginal)
-    if err != nil {
-        var t time.Time
-        return t, err
-    }
-    log.Println("Setting DateTimeOriginal to ", date.StringVal(), " on ", fname)
-    t, err := time.Parse("2006:01:02 15:04:05", date.StringVal())
-    if err != nil {
-        return time.Now(), err
-    }
+	date, err := x.Get(exif.DateTimeOriginal)
+	if err != nil {
+		return time.Now(), err
+	}
+	dateStr, err := date.StringVal()
+	if err != nil {
+		return time.Now(), nil
+	}
+	log.Println("Setting DateTimeOriginal to ", dateStr, " on ", fname)
+	t, err := time.Parse("2006:01:02 15:04:05", dateStr)
+	if err != nil {
+		return time.Now(), err
+	}
 
-    return t, nil
+	return t, nil
 }
 
 /*
@@ -237,19 +248,19 @@ getDestination builds a full path where the origPath should be copied to based
 on its DateTimeOriginal tag.
 */
 func (sp *Spool) getDestination(origPath, newBasePath string, t time.Time) string {
-    m := int(t.Month())
-    mon := strconv.Itoa(m)
+	m := int(t.Month())
+	mon := strconv.Itoa(m)
 
-    if m < 10 {
-        mon = "0" + mon
-    }
+	if m < 10 {
+		mon = "0" + mon
+	}
 
-    suffix := strings.ToLower(path.Ext(origPath))
-    if suffix == "jpeg" {
-        suffix = "jpg"
-    }
+	suffix := strings.ToLower(path.Ext(origPath))
+	if suffix == "jpeg" {
+		suffix = "jpg"
+	}
 
-    dir := filepath.Join(newBasePath, strconv.Itoa(t.Year()), mon)
-    fname := t.Format("2006-01-02_150405") + suffix
-    return filepath.Join(dir, fname)
+	dir := filepath.Join(newBasePath, strconv.Itoa(t.Year()), mon)
+	fname := t.Format("2006-01-02_150405") + suffix
+	return filepath.Join(dir, fname)
 }
